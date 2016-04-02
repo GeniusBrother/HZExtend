81783fcbb8b0bbfdb4b9812cdd80345b495ec37e
GeniusBrother <zuohong_xie@163.com>
2016-04-02 22:54:22 +0800

    头文件更新

--- Classes/HZExtend.h
+++ Classes/HZExtend.h
@@ -1,11 +1,15 @@
 //
-//  Extend.m
+//  HZExtend.m
 //  ZHFramework
 //
 //  Created by xzh. on 15/7/20.
 //  Copyright (c) 2015年 xzh. All rights reserved.
 //
 
+#ifndef _HZEXTEND_H_
+#define _HZEXTEND_H_
+
+#import "HZObserver.h"
 #import "NSString+HZExtend.h"
 #import "NSDictionary+HZExtend.h"
 #import "NSArray+HZExtend.h"
@@ -23,5 +27,7 @@
 #import "UIViewController+HZHUD.h"
 #import "HZURLManager.h"
 #import "HZViewController.h"
+
+#endif
 
 
